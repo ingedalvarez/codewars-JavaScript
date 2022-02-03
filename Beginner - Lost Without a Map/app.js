c468d1dc8d7d18147d96fb4861821ade7905834c
ingedalvarez <ingedalvarez@gmail.com>
2022-02-03 18:25:16 -0500

    Solved

---
+++ Beginner - Lost Without a Map/app.js
@@ -0,0 +1,11 @@
+function maps(x) {
+  return x.map((a) => a * 2);
+}
+
+console.log(maps([1, 2, 3, 4]));
+
+/* Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6] */
